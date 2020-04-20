5ad1a56370db6b22e49f17ed67c487b30288e330
John Rossi <DACOOK1983@gamil.com>
2020-04-20 13:23:52 -0400

    router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,41 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+//route?
+import {  Route, Link } from "react-router-dom";
 import SavedList from './Movies/SavedList';
 
-const App = () => {
-  const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
-  };
+//routes
+function Home (props) {
+  // debugger
+  return <h1>Home Comp</h1>;
+}
+
+function About (props) {
+  return <h1>About Comp</h1>;
+}
+const App = () => {
+  // const [savedList, setSavedList] = useState([]);
+
+  // const addToSavedList = movie => {
+  //   setSavedList([...savedList, movie]);
+  // };
 
   return (
     <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      
+      {/* <SavedList list={savedList} /> */}
+   
+     
+
+ 
+     
+     <Link to='/' >Home</Link>
+     <Link to='/about'>About</Link>
+
+<Route exact path="/home" component={Home} />
+     <Route exact path='/' component={Home} />
+     <Route exact path='/about' component={About}/>
+    
     </div>
   );
 };
